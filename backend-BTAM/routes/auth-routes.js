5bf75035b17bd83c19faf7fd6b4aed7f21c956b8
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-03-21 23:32:49 -0600

    add login, logout, loggdin

--- backend-BTAM/routes/auth-routes.js
+++ backend-BTAM/routes/auth-routes.js
@@ -57,4 +57,39 @@ authRoutes.post ('/signup', (req, res, next)=>{
   })
 })
 
+authRoutes.post('/login', (req,res, next)=>{
+  passport.authenticate('local',(err, theUser,failureDetails)=>{
+    if (err){
+      res.status(500).json({message:"Algo salio mal en la authentificacion"})
+      return
+    }
+    if(!theUser){
+       // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails)
+      return;
+    }
+    req.login(theUser, (err)=>{
+      if(err){
+        res.status(500).json({message:'La sesion se guardo mal'})
+        return
+      }
+      res.status(200).json(theUser)
+    })
+  })(req, res, next)
+})
+
+authRoutes.post('/logout', (req, res, next)=>{
+  req.logout();
+  res.status(200).json({message:'Se realizo el logout'})
+});
+
+authRoutes.get('/loggedin',(req,res,next)=>{
+  if(req.isAuthenticated()){
+    res.status(200).json(req.user)
+    return
+  }
+  res.status(403).json({message:'UnauthorizedN no esta logeado'})
+})
+
 module.exports = authRoutes;
